28e931ac9ce164b8dad3366aa168bd9effe3820a
Ayush Rawat <iamayushrawat@gmail.com>
2019-01-22 18:32:44 +0530

    Add files via upload

---
+++ ClientSocket/ClientSocket/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace client
+{
+    public class ClientSocket
+    {
+        private Socket clientSocket;
+        private int port;
+
+        public ClientSocket(int port)
+        {   //using Constructor to initiate the Socket
+            this.port = port;
+            Console.WriteLine("Starting client socket");
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.Title = "CLIENT SIDE";
+        }
+
+        // Separate function to Connect with the Server
+        public void connect() {
+            try
+            {
+                IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+                clientSocket.Connect(serverEndPoint);
+                Console.WriteLine("Coonected to Server");
+            }
+            catch (Exception e) {
+                Console.WriteLine("Error while connectiong to server {0}", e.Message);
+            }
+        }
+
+        // Function to start message transmission
+        public void start()
+        {
+            while (true) {
+                try
+                {
+                    //SEND----------------------------------------------------------
+                    Console.Write("Type a message: ");
+                    //Reading the data from Console Window
+                    String data = Console.ReadLine();
+                    
+                    //Encoding the data
+                    byte[] bytes = Encoding.UTF8.GetBytes(data);
+                    //using send function yo send the data
+                    clientSocket.Send(bytes);
+                    if (data == "Bye") {
+                        break;
+                    }
+                    //RECIEVE-------------------------------------------------------
+                    //Setting the Buffer size of recieved data
+                    int receiveBufferSize = clientSocket.ReceiveBufferSize;
+                    byte[] buffer = new byte[receiveBufferSize];
+
+                    int receivedBytes = clientSocket.Receive(buffer);
+                    byte[] receivedData = new byte[receivedBytes];
+
+                    for (int i = 0; i < receivedBytes; i++)
+                    {
+                        receivedData[i] = buffer[i];
+                    }
+
+                    String received = Encoding.UTF8.GetString(receivedData);
+
+                    Console.WriteLine("Server : {0}", received);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Connection termnated with error: {0}", e.Message);
+                    Console.WriteLine("Closing connection");
+                    clientSocket.Close();
+                    Console.Read();
+                }
+            }
+
+            Console.WriteLine("Closing connection");
+            clientSocket.Close();
+            Console.Read();
+        }
+    }
+
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            ClientSocket clientSocket = new ClientSocket(1234);
+            clientSocket.connect();
+            clientSocket.start();
+
+        }
+    }
+}
---
+++ ServerSocket/ServerSocket/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Server
+{
+    public class ServerSocket
+    {
+        private int port;
+        private Socket serverSocket;
+
+        public ServerSocket(int port)
+        {
+            this.port = port;
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+
+            /* Associates a Socket with a local endpoint. */
+            serverSocket.Bind(serverEndPoint);
+
+            /*Places a Socket in a listening state.
+             * The maximum length of the pending connections queue is 100 
+             */
+            serverSocket.Listen(100);
+        }
+
+        public void start()
+        {
+            Console.WriteLine("Starting the Server");
+            Console.Title = "SERVER SIDE";
+            /* Accept Connection Requests */
+            Socket accepted = serverSocket.Accept();
+
+            while (accepted.Connected) {
+                int bufferSize = accepted.SendBufferSize;
+                byte[] buffer = new byte[bufferSize];
+
+                /* Receives data from a bound Socket into a receive buffer. It return the number of bytes received. */
+                int bytesRead = accepted.Receive(buffer);
+
+                byte[] formatted = new byte[bytesRead];
+
+                for (int i = 0; i < bytesRead; i++)
+                {
+                    formatted[i] = buffer[i];
+                }
+
+                String receivedData = Encoding.ASCII.GetString(formatted);
+                
+                Console.WriteLine("Client: " + receivedData);
+                if(receivedData.Length.Equals(0)) {
+                    Console.WriteLine("Stopping Server");
+                    break;
+                }
+                Console.Write("Type a message: ");
+
+                String response = Console.ReadLine();
+                if (response == "Bye")
+                {
+                    Console.WriteLine("Stopping Server");
+                    break;
+                }
+                byte[] resp = Encoding.UTF8.GetBytes(response);
+                accepted.Send(resp, 0, resp.Length, 0);
+            }
+
+            serverSocket.Close();
+            Console.WriteLine("Press some key to close");
+            Console.Read();
+        }
+    }
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            ServerSocket server = new ServerSocket(1234);
+            server.start();
+        }
+    }
+}
